469d441c94456d4cf028ce1923571f7c6070ff9b
Carsten Friedrich <carsten.friedrich@csiro.au>
2018-04-28 16:24:53 +1000

    Started work on tutorial part 2, the MinMax algorithm

---
+++ util.py
@@ -0,0 +1,40 @@
+from IPython.display import HTML, display
+from tic_tac_toe.Player import Player
+from tic_tac_toe.Board import Board, GameResult, CROSS, NAUGHT
+
+
+def print_board(board):
+    display(HTML("""
+    <style>
+    .rendered_html table, .rendered_html th, .rendered_html tr, .rendered_html td {
+      border: 1px  black solid !important;
+      color: black !important;
+    }
+    </style>
+    """ + board.html_str()))
+
+
+def play_game(board: Board, player1: Player, player2: Player):
+    player1.new_game(CROSS)
+    player2.new_game(NAUGHT)
+    board.reset()
+
+    finished = False
+    while not finished:
+        result, finished = player1.move(board)
+        if finished:
+            if result == GameResult.DRAW:
+                final_result = GameResult.DRAW
+            else:
+                final_result = GameResult.CROSS_WIN
+        else:
+            result, finished = player2.move(board)
+            if finished:
+                if result == GameResult.DRAW:
+                    final_result = GameResult.DRAW
+                else:
+                    final_result = GameResult.NAUGHT_WIN
+
+    player1.final_result(final_result)
+    player2.final_result(final_result)
+    return final_result
