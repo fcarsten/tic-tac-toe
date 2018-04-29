19fb0112ed74c13d2967a96ba241969138ad675a
Carsten Friedrich <carsten.friedrich@csiro.au>
2018-04-29 15:08:13 +1000

    Added battle method to pit 2 players repeatedly against each other

--- util.py
+++ util.py
@@ -38,3 +38,24 @@ def play_game(board: Board, player1: Player, player2: Player):
     player1.final_result(final_result)
     player2.final_result(final_result)
     return final_result
+
+
+def battle(player1: Player, player2: Player, num_games: int = 100000):
+    board = Board()
+    draw_count = 0
+    cross_count = 0
+    naught_count = 0
+    for _ in range(num_games):
+        result = play_game(board, player1, player2)
+        if result == GameResult.CROSS_WIN:
+            cross_count += 1
+        elif result == GameResult.NAUGHT_WIN:
+            naught_count += 1
+        else:
+            draw_count += 1
+
+    print("After {} game we have draws: {}, Player 1 wins: {}, and Player 2 wins: {}.".format(num_games, draw_count,
+                                                                                         cross_count, naught_count))
+
+    print("Which gives percentages of draws: {:.2%}, Player 1 wins: {:.2%}, and Player 2 wins:  {:.2%}".format(
+        draw_count / num_games, cross_count / num_games, naught_count / num_games))
