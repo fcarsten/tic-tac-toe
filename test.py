11146354c3f4ade4a7dca99f7ca1ddd195a59222
Carsten Friedrich <carsten.friedrich@csiro.au>
2018-05-13 22:18:26 +1000

    Added code to start TensorBoard

--- test.py
+++ test.py
@@ -3,4 +3,8 @@ from tic_tac_toe.RandomPlayer import RandomPlayer
 from tic_tac_toe.MinMaxAgent import MinMaxAgent
 from tic_tac_toe.RndMinMaxAgent import RndMinMaxAgent
 
-battle(RandomPlayer(), RndMinMaxAgent())
+# battle(RandomPlayer(), RndMinMaxAgent())
+import tensorflow as tf
+from tensorboard import main as tb
+tf.flags.FLAGS.logdir = "./log"
+tb.main()
